dbe4cb3479101e909601c10997f1150538c795e7
donal_tong <donal.tong@gmail.com>
2014-06-30 23:15:53 +0800

    login ui

---
+++ app/src/main/java/com/donal/superne/app/ui/Welcome.java
@@ -0,0 +1,19 @@
+package com.donal.superne.app.ui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.donal.superne.app.BaseActivity;
+import com.donal.superne.app.R;
+
+public class Welcome extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+
+}
---
+++ app/src/main/java/com/donal/superne/app/ui/register/Register.java
@@ -0,0 +1,17 @@
+package com.donal.superne.app.ui.register;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.donal.superne.app.BaseActivity;
+import com.donal.superne.app.R;
+
+public class Register extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
