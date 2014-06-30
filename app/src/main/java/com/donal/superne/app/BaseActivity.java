83be0e63f2dd19793a2a5289df802f5a8bde104c
Donal <donal.tong@gmail.com>
2014-06-30 15:54:33 +0800

    create

---
+++ app/src/main/java/com/donal/superne/app/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.donal.superne.app;
+
+import android.app.Activity;
+
+/**
+ * Created by donal on 14-6-30.
+ */
+public class BaseActivity extends Activity {
+}
---
+++ app/src/main/java/com/donal/superne/app/BaseApplication.java
@@ -0,0 +1,9 @@
+package com.donal.superne.app;
+
+import android.app.Application;
+
+/**
+ * Created by donal on 14-6-30.
+ */
+public class BaseApplication extends Application {
+}
