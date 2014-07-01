0adf79921e3482782cfec4441a2cb0615f0392bf
donal_tong <donal.tong@gmail.com>
2014-07-01 22:31:14 +0800

    register

---
+++ app/src/main/java/com/donal/superne/app/ui/register/RegisterPass.java
@@ -0,0 +1,18 @@
+package com.donal.superne.app.ui.register;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.donal.superne.app.BaseActivity;
+import com.donal.superne.app.R;
+
+public class RegisterPass extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_pass);
+    }
+
+}
